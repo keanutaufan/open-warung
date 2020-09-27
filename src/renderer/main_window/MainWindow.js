8d5138800653d4de2ca5cfff987c78cf56bbff88
Keanu Taufan <keanu.taufan21@gmail.com>
2020-09-27 21:02:39 +0700

    Navigasi menu melalui navbar

--- src/renderer/main_window/MainWindow.js
+++ src/renderer/main_window/MainWindow.js
@@ -6,3 +6,12 @@ Component.register('components/barang.html', 'barang');
 Component.register('components/arus-kas.html', 'arus-kas');
 Component.register('components/pengaturan.html', 'pengaturan');
 Component.register('components/bantuan.html', 'bantuan');
+
+
+const menuNavigate = index => {
+    document.getElementsByClassName('menu-item selected')[0].classList.remove('selected');
+    document.getElementsByClassName('menu-item')[index].classList.add('selected');
+
+    document.getElementsByClassName('content-component visible')[0].classList.remove('visible');
+    document.getElementsByClassName('content-component')[index].classList.add('visible');
+}
