36cb719638ad9b3957e08e9ee1b9e03147bb6e69
Keanu Taufan <keanu.taufan21@gmail.com>
2020-11-01 16:47:56 +0700

    Isolasi scope CSS untuk layout barang
    Untuk mencegah style berlaku pada layout yang bukan seharusnya
    TODO: Refactor CSS dan ganti nama dengan penamaan yang konsisten

--- src/renderer/main_window/ItemTable.js
+++ src/renderer/main_window/ItemTable.js
@@ -6,29 +6,29 @@ module.exports = {
         let table = document.getElementById('barang-table');
 
         let DOMString = `
-            <tr>
-                <th>Nama Barang</th>
-                <th>Stok</th>
-                <th>Min</th>
-                <th>Harga Beli</th>
-                <th>Harga Jual</th>
-                <th></th>
-                <th></th>
-                <th></th>
+            <tr class="barang-table-header barang-table-row">
+                <th class="barang-table-heading">Nama Barang</th>
+                <th class="barang-table-heading">Stok</th>
+                <th class="barang-table-heading">Min</th>
+                <th class="barang-table-heading">Harga Beli</th>
+                <th class="barang-table-heading">Harga Jual</th>
+                <th class="barang-table-heading"></th>
+                <th class="barang-table-heading"></th>
+                <th class="barang-table-heading"></th>
             </tr>
         `;
 
         data.forEach((element, index) => {
             DOMString += `
-                <tr>
-                    <td class="data-visualizer">${element.barang}</td>
-                    <td class="data-visualizer">${element.stok}</td>
-                    <td class="data-visualizer">${element.min}</td>
-                    <td class="data-visualizer">${new Intl.NumberFormat('id').format(element.beli)}</td>
-                    <td class="data-visualizer">${new Intl.NumberFormat('id').format(element.jual)}</td>
-                    <td class="table-button"><span onclick="addItem(${index})" class="btn-add"><img src="../../assets/icons/btn-add.svg" class="img-add"></span></td>
-                    <td class="table-button"><span onclick="subtractItem(${index})" class="btn-subtract"><img src="../../assets/icons/btn-subtract.svg" class="img-subtract"></span></td>
-                    <td class="table-button"><span onclick="removeItem(${index})" class="btn-delete"><img src="../../assets/icons/btn-delete.svg" class="img-delete"></span></td>
+                <tr class="barang-table-row">
+                    <td class="barang-table-data" data-visualizer">${element.barang}</td>
+                    <td class="barang-table-data" data-visualizer">${element.stok}</td>
+                    <td class="barang-table-data" data-visualizer">${element.min}</td>
+                    <td class="barang-table-data" data-visualizer">${new Intl.NumberFormat('id').format(element.beli)}</td>
+                    <td class="barang-table-data" data-visualizer">${new Intl.NumberFormat('id').format(element.jual)}</td>
+                    <td class="barang-table-data" table-button"><span onclick="addItem(${index})" class="btn-add"><img src="../../assets/icons/btn-add.svg" class="img-add"></span></td>
+                    <td class="barang-table-data" table-button"><span onclick="subtractItem(${index})" class="btn-subtract"><img src="../../assets/icons/btn-subtract.svg" class="img-subtract"></span></td>
+                    <td class="barang-table-data" table-button"><span onclick="removeItem(${index})" class="btn-delete"><img src="../../assets/icons/btn-delete.svg" class="img-delete"></span></td>
                 </tr>
             `;
         });
