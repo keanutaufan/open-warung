c9d89831a64fde87257356068f6d37625f9c8e9e
Keanu Taufan <keanu.taufan21@gmail.com>
2020-11-06 14:07:57 +0700

    Pendaftaran ikon edit barang di tabel

--- src/renderer/main_window/ItemTable.js
+++ src/renderer/main_window/ItemTable.js
@@ -28,6 +28,7 @@ module.exports = {
                     <td class="barang-table-data" data-visualizer">${new Intl.NumberFormat('id').format(element.jual)}</td>
                     <td class="barang-table-data" table-button"><span onclick="addItem(${index})" class="btn-add"><img src="../../assets/icons/btn-add.svg" class="img-add"></span></td>
                     <td class="barang-table-data" table-button"><span onclick="subtractItem(${index})" class="btn-subtract"><img src="../../assets/icons/btn-subtract.svg" class="img-subtract"></span></td>
+                    <td class="barang-table-data" table-button"><span onclick="editItem(${index})" class="btn-edit"><img src="../../assets/icons/btn-edit.svg" class="img-edit"></span></td>
                     <td class="barang-table-data" table-button"><span onclick="removeItem(${index})" class="btn-delete"><img src="../../assets/icons/btn-delete.svg" class="img-delete"></span></td>
                 </tr>
             `;
