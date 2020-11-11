eb10605097eedb96e3340525c816066af8e80f14
Keanu Taufan <keanu.taufan21@gmail.com>
2020-11-11 14:38:11 +0700

    Fitur rendering list catatan

--- src/renderer/main_window/catatan/Notes.js
+++ src/renderer/main_window/catatan/Notes.js
@@ -25,7 +25,46 @@ const remove = index => {
     Storage.removeNote(index);
 }
 
+const generateHTML = (data, index) => {
+    const monthName = [
+        'ERROR', 'Januari', 'Februari', 'Maret',
+        'April', 'Mei', 'Juni', 'Juli', 'Agustus',
+        'September', 'Oktober', 'November', 'Desember'
+    ];
+
+    return `
+        <div class="card notes-card">
+            <div class="card-pemasukan-header">
+                <div class="card-title" style="text-transform: capitalize">${data.title}</div>
+            </div>
+            <div class="card-body notes-body">
+                ${data.text}
+            </div>
+            <div class="card-footer notes-footer">
+                <div class="card-time">
+                    ${data.time.date} ${monthName[data.time.month]} ${data.time.year} 
+                    ${data.time.hour}:${data.time.minute}
+                </div>
+                <div class="card-button-container">
+                    <button class="card-delete-pengeluaran" onclick="removeNote(${index})">Hapus</button>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+const render = () => {
+    let DOMString = '';
+
+    for (let i = Storage.appData.notes.length - 1; i >= 0; i--) {
+        DOMString += generateHTML(Storage.appData.notes[i], i);
+    }
+    document.getElementById('catatan-data-list').innerHTML = DOMString;
+}
+
 module.exports = {
     add: add,
-    remove: remove
+    remove: remove,
+    generateHTML: generateHTML,
+    render: render
 }
